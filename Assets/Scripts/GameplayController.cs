452e8120b18738e4f107f8aa029056ed25ca1332
Henry Moss <hdamoss@gmail.com>
2025-07-21 15:23:50 -0400

    Updated gameplay loop for triggering power outages; added placeholder text for phase dialogue; updated fuse model to be more visible; fixed an issue where the light flicker was not toggling lights correctly

--- Assets/Scripts/GameplayController.cs
+++ Assets/Scripts/GameplayController.cs
@@ -45,16 +45,15 @@ public class GameplayController : MonoBehaviour
             //Spawn Zombies
             if (phase >= 2)
             {
-                for (int i = 0; i < phase; i++)
-                {
-                    GameObject zombieObj = Instantiate(zombiePrefab, zombieSpawns[Random.Range(0, zombieSpawns.Count - 1)].position, Quaternion.identity);
-                }
+                //GameObject zombieObj = Instantiate(zombiePrefab, zombieSpawns[Random.Range(0, zombieSpawns.Count - 1)].position, Quaternion.identity);
+                DialogueController.instance.UpdateText("[TODO]: Spawn zombie");
             }
 
             //Spawn Static Man
             if (phase >= 3)
             {
-                GameObject staticManObj = Instantiate(staticManPrefab, staticManSpawns[Random.Range(0, staticManSpawns.Count - 1)].position, Quaternion.identity);
+                //GameObject staticManObj = Instantiate(staticManPrefab, staticManSpawns[Random.Range(0, staticManSpawns.Count - 1)].position, Quaternion.identity);
+                DialogueController.instance.UpdateText("[TODO]: Spawn static man");
             }
         }
     }
@@ -66,16 +65,19 @@ public class GameplayController : MonoBehaviour
             case 0:
                 //Initial tutorial
                 //Radio gives initial message from supervisor explaining how to play
+                DialogueController.instance.UpdateText("[TODO]: Put intro dialogue here");
                 break;
             case 1:
                 //Introduce power outages
                 //Radio gives another message about the power outage, directs the player to use the fuse box to restore power
                 //Quick look at zombie enemy as lights come back
+                DialogueController.instance.UpdateText("[TODO]: Add dialogue talking about power outages and how to fix them");
                 break;
             case 2:
                 //Power outage starts on a timer
                 //If power is out, spawn zombie and have them move closer during each outage
                 //If zombie touches player, game over
+                DialogueController.instance.UpdateText("[TODO]: Have radio voice talk about strange readings in the area");
                 break;
             case 3:
                 //Radio message is garbled and more ominous
@@ -83,11 +85,14 @@ public class GameplayController : MonoBehaviour
                 //If the player is too stressed, start spawning Static man
                 //Player must shred documents to lower stress (all documents while stressed must be shredded)
                 //Power outages still happen on a timer
+                DialogueController.instance.UpdateText("[TODO]: Radio voice starts talking about a dark king and black skies, 'he is coming. listen for his Voice'");
                 break;
             case 4:
                 //Final scenario
+                DialogueController.instance.UpdateText("[TODO]: Add final dialogue from radio explaining that the rest of the site is falling and that the player has to finish the work");
                 break;
             default:
+                DialogueController.instance.UpdateText("[TODO]: Default Scenario Text");
                 break;
         }
     }
@@ -96,15 +101,19 @@ public class GameplayController : MonoBehaviour
     {
         score++;
 
-        //int randVal = Random.Range(3, 8);
-        if (score >= 1)//randVal)
+        int randVal = Random.Range(3, 8);
+        if (score >= randVal)
         {
-            print("TODO: flicker lights as they go off");
-            powerOutage = true;
-            if (poRoutine == null)
-                poRoutine = StartCoroutine(PowerOutageRoutine(false));
-            FuseBox.instance.SetBroken();
-            score = 0;
+            phase++;
+
+            if (phase > 0)
+            {
+                powerOutage = true;
+                if (poRoutine == null)
+                    poRoutine = StartCoroutine(PowerOutageRoutine(false));
+                FuseBox.instance.SetBroken();
+                score = 0;
+            }
         }
     }
 
@@ -129,15 +138,16 @@ public class GameplayController : MonoBehaviour
     {
         foreach (Light light in lights)
         {
+            light.enabled = true;
             light.GetComponent<LightFlicker>().enabled = true;
         }
 
-        yield return new WaitForSeconds(0.75f);
+        yield return new WaitForSeconds(1.5f);
 
         foreach (Light light in lights)
         {
             light.enabled = turnLightsOn;
-            //light.GetComponent<LightFlicker>().enabled = false;
+            light.GetComponent<LightFlicker>().enabled = false;
         }
 
         poRoutine = null;
