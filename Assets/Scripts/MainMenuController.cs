a8c6338163bb7124b2e95c6a1d61f93695db55cb
Henry Moss <hdamoss@gmail.com>
2025-07-31 11:22:54 -0400

    Updated new main menu UI layout; updated start game logic; implemented screen fade logic; added a version number text; fixed an issue with the build settings missing certain scenes

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,23 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TMP_Text versionNumber;
 
-    // Update is called once per frame
-    void Update()
+    bool startingGame;
+    Coroutine startRoutine;
+
+    private void Start()
     {
-        
+        startingGame = false;
+        startRoutine = null;
+
+        versionNumber.text = $"v{Application.version}";
+
+        FadeController.instance.StartFade(0f, 1f);
     }
 
     public void StartGame()
     {
+        if (!startingGame
+            && !FadeController.instance.isFading
+            && startRoutine == null)
+        {
+            startingGame = true;
+            startRoutine = StartCoroutine(StartGameRoutine());
+        }
+    }
+
+    IEnumerator StartGameRoutine()
+    {
+        FadeController.instance.StartFade(1f, 1.5f);
+
+        while (FadeController.instance.isFading)
+            yield return null;
+
+        yield return new WaitForSeconds(1f);
+
         SceneManager.LoadScene(1);
+
+        startRoutine = null;
+    }
+
+    public void Settings()
+    {
+        if (!startingGame && !FadeController.instance.isFading)
+        {
+            print("Open settings menu here");
+        }
+    }
+
+    public void Credits()
+    {
+        if (!startingGame && !FadeController.instance.isFading)
+        {
+            print("Open credits menu here");
+        }
     }
 }
